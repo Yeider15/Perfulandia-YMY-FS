7daf9552b8c18d3c0dbd2e2f5d493fd062c05c82
Yeider15 <ye.catari@duocuc.cl>
2025-05-22 12:41:14 -0400

    Proyeco subido

---
+++ src/main/java/com/ymy/cl/perfulandiafs/PerfulandiafsApplication.java
@@ -0,0 +1,13 @@
+package com.ymy.cl.perfulandiafs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfulandiafsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfulandiafsApplication.class, args);
+	}
+
+}
